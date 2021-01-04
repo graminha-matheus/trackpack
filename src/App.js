643479cf1a2779f3d986c2a45acdf257b58d6846
Graminha <matheus.moreli@hotmail.com>
2021-01-04 10:24:52 -0300

    Iniciando a utilizaçao do Postmon API

--- src/App.js
+++ src/App.js
@@ -2,32 +2,33 @@ import './GlobalStyle.css'
 import Footer from './Components/Footer/index';
 
 import Header from './Components/Header/index';
-import { Component } from 'react';
-/* import Form from './Components/Form/index';
- */
+import React from 'react';
+import Form from './Components/Form/index';
+
+
 function App() {
+
+
+
   return (
     <div className="App">
       <Header />
-      <Footer/>
+      <Form/>
+      <Footer/> 
       {/* <Form /> */}
     </div>
   );
 }
 
-class App extends Component {
+/* class App extends Component {
   state = {
     trackData: []
   }
   componentDidMount(){
-      fetch('https://api.linketrack.com/track/json?user=teste&token=1abcd00b2731640e886fb41a8a9671ad1434c599dbaa0a0de9a5aa619f29a83f&codigo=LX002249507BR')
-      .then(trackResult => trackResult.json())
-      .then((data) => {
-        this.setState({ trackData: data })
-      })
-      .catch(console.log)
+
+    
     }
     
-}
+} */
 
 export default App;
--- src/Components/Form/index.js
+++ src/Components/Form/index.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './style.css';
 
 const Form = () => {
-    
+const {data, setData} = useState([])
+
+    /* Puxa a API do codigo de rastreio */
+    fetch('https://api.postmon.com.br/v1/rastreio/ect/' + data)
+    /* Passa os resultados para JSON */
+    .then(trackResult => trackResult.json())
+    /* Joga os resultados para o trackData por meio do setState */
+    .then((data) => {
+      setData(data)
+    })
+    /* Faz os resultados aparecerem no console caso dê erro (Else) */
+    .catch(console.log('Erro.'))
+    console.log(data)
+
     return (
         <div className="search-bar">
-{           <input type="text" id="pesquisa" placeholder="Código de rastreio"></input>
+{           <label>
+            <input type="text" id="pesquisa" placeholder="Código de rastreio"></input>
+            </label>
 }       </div>
     )
 }
 
-export default Header;
-
+export default Form;
