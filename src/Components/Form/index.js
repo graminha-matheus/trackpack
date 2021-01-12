ca08527c002482e1487b7d1b111677493fd2aa3d
Raul Franck <raul.franck@bcrcx.com>
2021-01-12 10:11:04 -0300

    estilizaçao segunda pagina

--- src/Components/Form/index.js
+++ src/Components/Form/index.js
@@ -73,77 +73,84 @@ const [hasit, setHasLoaded] = useState(false);
 
                 <div className = {hasit === true ? 'show_info' : 'dontshow_info'}>
 
-                    <div>
-                        <h4 className = {hasit === true ? 'show_info' : 'dontshow_info'}> 
-                        Código de rastreio: 
-                        </h4>
+                    
+                    {/* A partir daqui, form de retorno */}
+                    
+                    
+                    <div className="response-container">
 
-                        <input 
-                        value={end.codigo} 
-                        className = {hasit === true ? 'show_info' : 'dontshow_info'} 
-                        disabled
-                        >
-                        </input> 
-                    </div>
-                          
-                    <div>
-                        <h4 className = {hasit === true ? 'show_info' : 'dontshow_info'}>  
-                        Status atual do pedido: 
-                        </h4> 
-
-                        <input 
-                        value={hasit === true ? end.eventos[0].status : error} 
-                        className = {hasit === true ? 'show_info' : 'dontshow_info'} 
-                        disabled
-                        >
-
-                        </input> 
-
-                        <span className = {hasit === true ? 'show_info' : 'dontshow_info'}>
-                            <h4>
-                                Histórico: 
+                    
+                        <div className="code-input">
+                            <h4 className = {hasit === true ? 'show_info' : 'dontshow_info'}> 
+                            Código de rastreio: 
                             </h4>
-                        </span>
 
-                        <div className="scroll">
+                            <input 
+                            value={end.codigo} 
+                            className = {hasit === true ? 'show_info' : 'dontshow_info'} 
+                            disabled
+                            >
+                            </input> 
+                        </div>
+                            
+                        <div className="code-input">
+                            <h4 className = {hasit === true ? 'show_info' : 'dontshow_info'}>  
+                            Status atual do pedido: 
+                            </h4> 
 
-                            <table className={hasit === true ? 'show_info' : 'dontshow_info'}>
-                                {hasit === true ? end.eventos.map(item => { 
-                                    
-                                        return (
-                                            <tbody>
-                                                <tr className="info1">
-                                                    <td>{item.data}, às {item.hora} <br /> Em {item.local}<br /> 
-                                                    {item.status}. <br /> 
-                                                    {item.subStatus[1]}</td>
-                                                </tr>
-                                            </tbody>
-                                        )
+                            <input 
+                            value={hasit === true ? end.eventos[0].status : error} 
+                            className = {hasit === true ? 'show_info' : 'dontshow_info'} 
+                            disabled
+                            >
 
-                                }) : error }
-                            </table>
+                            </input> 
+
+                            <span className = {hasit === true ? 'show_info' : 'dontshow_info'}>
+                                <h4>
+                                    Histórico: 
+                                </h4>
+                            </span>
+
+                            <div className="scroll">
+
+                                <table className={hasit === true ? 'show_info' : 'dontshow_info'}>
+                                    {hasit === true ? end.eventos.map(item => { 
+                                        
+                                            return (
+                                                <tbody>
+                                                    <tr className="info1">
+                                                        <td>{item.data}, às {item.hora} <br /> Em {item.local}<br /> 
+                                                        {item.status}. <br /> 
+                                                        {item.subStatus[1]}</td>
+                                                    </tr>
+                                                </tbody>
+                                            )
+
+                                    }) : error }
+                                </table>
+                            </div>
+
                         </div>
 
-                    </div>
-
-                    <div className="btn">
-                        <button className = {hasit === true ? 'show_info' : 'dontshow_info'}
-                                type="submit" 
-                                onClick={() => setHasLoaded(false)}
-                                alt="Nova Consulta"
-                            > 
-                            <AiOutlineRedo size="40"></AiOutlineRedo>
+                        <div className="btn">
+                            <button className = {hasit === true ? 'show_info' : 'dontshow_info'}
+                                    type="submit" 
+                                    onClick={() => setHasLoaded(false)}
+                                    alt="Nova Consulta"
+                                > 
+                                <AiOutlineRedo size="40"></AiOutlineRedo>
+                                
+                            </button>
                             
-                        </button>
-                        
-                        <button className = {hasit === true ? 'show_info' : 'dontshow_info'}
-                                type="submit" 
-                                onClick={handlerSubmit}
-                                alt="Aplicar no Ticket"
-                            > 
-                            <AiOutlinePlus size="40"></AiOutlinePlus>
-                            
-                        </button>
+                            <button className = {hasit === true ? 'show_info' : 'dontshow_info'}
+                                    type="submit" 
+                                    onClick={handlerSubmit}
+                                    alt="Aplicar no Ticket"
+                                > 
+                                <AiOutlinePlus size="40"></AiOutlinePlus>
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
